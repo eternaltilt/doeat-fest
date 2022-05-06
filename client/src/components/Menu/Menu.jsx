48ff3c46eb7401ae4a4becdda692414e73296089
tilt <efejypov@gmail.com>
2022-05-06 16:44:09 +0300

    burgerfix

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import './Menu.css';
 
